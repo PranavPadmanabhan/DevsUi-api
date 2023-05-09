13c4c1364760fb5dd1a7e0afb95e880a9e32b131
PranavPadmanabhan <PranavPadmanabhan77@gmail.com>
2023-05-09 11:00:11 +0530

    updated readme

--- routes/Conversations.js
+++ routes/Conversations.js
@@ -33,7 +33,7 @@ router.post("/", async (req, res) => {
 
 router.get("/:address", async(req, res) => {
     try {
-        const conversations = await Conversations.find({ members: { $in: [req.params.address] } });
+        const conversations = await Conversations.find({ members: { $in: [{address:req.params.address,name:req.query.name}] } });
         if(conversations){
             res.status(201).json(conversations)
         }
